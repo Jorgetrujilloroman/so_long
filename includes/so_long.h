44dc286e3dd3ae3bbbccc7c474ab9dd1cfe568d7
Jorge <jorgetrujilloroman@gmail.com>
2024-08-21 18:06:52 +0200

    TODO: keep working on main

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,22 @@
 /*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 17:48:31 by jotrujil          #+#    #+#             */
-/*   Updated: 2024/08/20 17:48:32 by jotrujil         ###   ########.fr       */
+/*   Updated: 2024/08/21 18:05:41 by jotrujil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+# include "../lib/libft/libft.h"
+
+typedef struct s_game
+{
+	char	**grid;
+	size_t	width;
+	size_t	height;
+
+}	t_game
+
+#endif
